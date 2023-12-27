fb65ac932ca8f0995c20bad4fe4bdaa3aaf7d601
ners <ners@gmx.ch>
2023-12-27 18:33:12 +0100

    patch up textinput

--- dunai-imgui/lib/FRP/Dunai/DearImGui/Widgets/InputText.hs
+++ dunai-imgui/lib/FRP/Dunai/DearImGui/Widgets/InputText.hs
@@ -5,11 +5,7 @@ module FRP.Dunai.DearImGui.Widgets.InputText where
 
 import Data.MonadicStreamFunction (iPre)
 import Data.Tuple.Extra (uncurry3)
-import Data.Unique (hashUnique)
-import Data.Unique qualified as Data
-import DearImGui qualified
 import DearImGui qualified as DearImgui
-import DearImGui.Raw qualified as DearImGui
 import Internal.Prelude
 
 data InputText = InputText
@@ -29,18 +25,12 @@ type IsInputText b =
 inputText' :: forall m t. (MonadGUI m, IsInputText t) => MSF m t t
 inputText' = proc t -> do
     ref <- arrM newIORef -< t.value
-    changed <- arrM mkInputText -< (t.label, ref, 99999)
+    -- FIXME input text callbacks are not supported in dear-imgui.hs.
+    -- Changes must be made to the library itself and upstreamed
+    changed <- arrM (uncurry3 DearImgui.inputText) -< (t.label, ref, 99999)
     value <- arrM readIORef -< ref
-    let modify = #changed .~ changed >>> #value .~ value
+    let modify = #changed .~ changed <<< #value .~ value
     returnA -< modify t
-  where
-    mkInputText :: (Text, IORef Text, Int) -> m Bool
-    mkInputText args = do
-        id <- fromIntegral . hashUnique <$> liftIO Data.newUnique
-        DearImGui.pushIDInt id
-        res <- DearImGui.withID id $ uncurry3 DearImgui.inputText args
-        liftIO DearImGui.popID
-        pure res
 
 -- | TODO Cajun will write nice docs here :-)
 inputTextCustom :: forall m t. (MonadGUI m, IsInputText t) => MSF m t t -> t -> MSF m () t
